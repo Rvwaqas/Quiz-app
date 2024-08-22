d182a0256140b5fab29e558cf3a133c4b369771a
Rv waqas <145973183+Rvwaqas@users.noreply.github.com>
2024-08-22 23:18:38 +0500

    Add files via upload
    add

---
+++ index.ts
@@ -0,0 +1,44 @@
+import inquirer from 'inquirer'
+import chalk from 'chalk'
+
+
+const apiLink:string="https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple";
+
+let fetchData= async (data:string)=>{
+let fetchQuiz:any=await fetch(data)
+let res=await fetchQuiz.json()
+return res;
+}
+
+let data=await fetchData(apiLink);
+
+let startQuiz= async ()=>{
+    let score:number=0;
+    //for user name
+    let name=await inquirer.prompt({
+        type:"input",
+        name:"fname",
+        message:"Enter your name?"
+    })
+
+    for(let i=1;i<=5;i++){
+        let answer:any[]=[...data[i].incorrect_answers,data[i].correct_answer]
+    
+        let ans=await inquirer.prompt({
+            type:"list",
+            name:"quiz",
+            message:data[i].question,
+            choices:answer.map((val)=> val),
+        });
+
+        if(ans.quiz==data[i].correct_answer){
+            ++score
+        }else{
+            console.log(`${--score} your answer is wornd`)
+        }
+
+    }
+    console.log(`Dear ${name.fname} your score is ${chalk.bold(score)}`);
+     
+}
+startQuiz();
